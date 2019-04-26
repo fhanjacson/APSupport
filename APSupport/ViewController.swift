be05fe1a1d40ff6efdfb5c3066fa4a7442b82f63
localadmin <localadmin@tl04-06-09.local>
2019-04-26 11:54:02 +0800

    Initial Commit

---
+++ APSupport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APSupport
+//
+//  Created by localadmin on 26/04/2019.
+//  Copyright © 2019 Fhan Jacson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
